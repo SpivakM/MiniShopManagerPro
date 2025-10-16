875569f978e72ff759a8deb751b4cbd40ee1ef5d
Anna Husarova <anna.husarova.shi.2025@lpnu.ua>
2025-10-16 14:16:56 +0300

    total_price

--- dependencies/total_price.cpp
+++ dependencies/total_price.cpp
@@ -1,5 +1,11 @@
 #include "total_price.h"
 
 void total_price(vector<Item> items) {
+    double total = 0.0;
+    for (const auto& item : items) {
+        total += item.price * item.amount;
+    }
+
+    cout << "Загальна вартість складу: " << total << " грн\n";
 
 }
