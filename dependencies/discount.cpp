a3fa728af72791d82f2816b7a002bcd468197187
pikveronika <veronika.pik.shi.2025@lpnu.ua>
2025-10-17 20:24:43 +0300

    add: discount function

--- dependencies/discount.cpp
+++ dependencies/discount.cpp
@@ -1,5 +1,23 @@
 #include "discount.h"
 
+
 void discount(vector<Item> items) {
+
+    bool found = false;
+    for (auto& item : items) {
+        float price = item.price;
+        
+
+        if (price > 1000) {
+            price *= 0.9; 
+            item.price = price;
+            cout << "Product \"" << item.name << "\" got a 10% discount. New price: " << price << " UAH\n";
+            found = true;
+        }
+    }
+
+    if (!found) {
+        cout << "No products over 1000 UAH found.\n";
+    }
 
 }
