5be5280893fba1d6047bcb40ad756446ce6ac13c
Disp <dimaspivak1602@gmail.com>
2025-10-17 13:36:26 +0300

    Changes to print_list_of_commands()

--- shop.cpp
+++ shop.cpp
@@ -9,8 +9,7 @@ int main(void) {
 	int command;
 	bool cont = true;
 
-	printf("--- Menu ---\n");
-	printf("LIST OF COMMANDS\n");
+	print_list_of_commands();
 
 	vector<Item> items;
 
@@ -74,15 +73,16 @@ void print_list_of_commands() {
     printf("LIST OF COMMANDS\n");
     printf(
         "0 - Exit\n"
-        "1 - Help\n"
+        "1 - Menu\n"
         "2 - Add item\n"
         "3 - Buy item\n"
         "4 - Change price of item\n"
         "5 - Delete item\n"
         "6 - Discount\n"
         "7 - Find item by name\n"
-        "8 - List items\n"
-        "9 - List items with price under 100\n"
-        "10 - Total price of items\n");
+		"8 - Find item with max price\n"
+        "9 - List items\n"
+        "10 - List items with price under 100\n"
+        "11 - Total price of items\n");
 	
 }
