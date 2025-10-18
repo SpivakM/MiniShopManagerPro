f4b3d10cc495252e7a2c6ac4f8155cff224f3a8e
KostikKotiK <kostiantyn.harashchak.shi.2025@lpnu.ua>
2025-10-18 18:52:34 +0300

    Fix: Removed the ability to add items multiple times

--- dependencies/add_item.cpp
+++ dependencies/add_item.cpp
@@ -1,60 +1,43 @@
 #include "add_item.h"
 
 void add_item(vector<Item>& items) {
-	int Choose = 0;
-    while (true) {
-        cout << "  Menu" << endl;
-        cout << "1 - Enter a product\n2 - Exit" << endl;
-        if (!( cin >> Choose)) {
+    Item it;
+    cout << "Enter the product name: ";
+    cin.ignore();
+    getline(cin, it.name);
+                
+    cout << "Enter price: ";
+    while(true) {
+        if(!(cin >> it.price)) {
             cin.clear();
             cin.ignore(10000, '\n');
+            cout << "\nEnter numeric price: ";
             continue;
         }
 
-        if(Choose == 1) {
-            Item it;
-            cout << "Enter the product name: ";
-            cin.ignore();
-            getline(cin, it.name);
-            
-            cout << "Enter price: ";
-            while(true) {
-                if(!(cin >> it.price)) {
-                    cin.clear();
-                    cin.ignore(10000, '\n');
-                    cout << "\nEnter numeric price: ";
-                    continue;
-                }
-
-                if(it.price < 0) {
-                    cout << "\nPrice cannot be negative: ";
-                    continue;
-                }
-                break;
-            }
-
-            cout << "Enter amount of product: ";
-            while (true) {
-                if(!(cin >> it.amount)) {
-                    cin.clear();
-                    cin.ignore(10000, '\n');
-                    cout << "\nEnter numeric amount: ";
-                    continue;
-                }
-
-                if(it.amount < 0) {
-                    cout << "\nAmount cannot be negative: ";
-                    continue;
-                }
-                break;
-            }
-
-            items.push_back(it);
-            cout << "Product added.\n";
+        if(it.price < 0) {
+            cout << "\nPrice cannot be negative: ";
+            continue;
         }
-
-        if(Choose == 2) {
-            break;
-        }
+        break;
     }
+
+    cout << "Enter amount of product: ";
+    while (true) {
+        if(!(cin >> it.amount)) {
+            cin.clear();
+            cin.ignore(10000, '\n');
+            cout << "\nEnter numeric amount: ";
+            continue;
+        }
+
+        if(it.amount < 0) {
+            cout << "\nAmount cannot be negative: ";
+            continue;
+        }
+        break;
+    }
+
+    items.push_back(it);
+    cout << "Product added.\n";
 }
