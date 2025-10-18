9c0c69c59ebf6d1d31be746790042ef936b7f88d
SinchukTymofii <tymofii.sinchuk.shi.2025@lpnu.ua>
2025-10-18 14:31:06 +0300

    buy_item

--- dependencies/buy_item.cpp
+++ dependencies/buy_item.cpp
@@ -1,5 +1,28 @@
 #include "buy_item.h"
 
 void buy_item(vector<Item>& items) {
-
+    string n;
+    cout << "Enter name of Item"<<endl;
+    cin >> n;
+    bool t=true;
+    for(int i=0; i < items.size(); i++)
+    {
+        if(items[i].name == n)
+        {
+            if(items[i].amount > 0)
+            {
+                items[i].amount--;
+                cout << "Item bought"<<endl;
+            }
+            else
+                cout << "Item not avaible"<<endl;
+            t = false;
+            break;
+        }   
+    }
+    if(t)
+    {
+        cout << "There is no such item"<<endl;
+    }
+    
 }
