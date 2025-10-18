5cb1013309da49cb59c2e56b464fb9bef808d8e6
AskoldKushta <askold.kushna.shi.2025@lpnu.ua>
2025-10-18 23:05:29 +0300

    fixed func

--- dependencies/find_item_with_max_price.cpp
+++ dependencies/find_item_with_max_price.cpp
@@ -1,26 +1,22 @@
 #include "find_item_with_max_price.h"
 
-void find_item_with_max_price(vector<Item> items) {
+void find_item_with_max_price(vector<Item>& items) {
 
-    vector<int> max_price_item_index;
-    max_price_item_index[0]=0;
-    int c=0;
+    int max_price=0;
 
-    for (int i=1; i<items.size(); i++){
+    for (int i=0; i<items.size(); i++){
         
-        if (items[i].price > items[i-1].price){
+        if (items[i].price > max_price){
 
-            max_price_item_index[c]=i;
-            c++;
-        }
-        else if (items[i].price == items[i-1].price){
-            
-            max_price_item_index[c]=i;
-            c++;
+            max_price=items[i].price;
         }
     }
 
-    for (int i=0; i<c; i++){
-        cout << items[i].name << endl;
+    for (int i=0; i<items.size(); i++){
+
+        if (items[i].price == max_price){
+
+            cout << items[i].name << endl;
+        }
     }
 }
