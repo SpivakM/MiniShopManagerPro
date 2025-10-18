05db91a9b1ff6f9951710a7ed6dc2ff55f2f377f
zlatiok <zlatastar01@gmail.com>
2025-10-18 17:18:53 +0300

    change_item_price_ver1

--- dependencies/change_item_price.cpp
+++ dependencies/change_item_price.cpp
@@ -1,5 +1,29 @@
 #include "change_item_price.h"
 
 void change_item_price(vector<Item>& items) {
+    if (items.empty()) {
+        cout << "No items available.\n";
+        return;
+    }
 
+    string name;
+    cout << "Enter product name: ";
+    cin.ignore();
+    getline(cin, name);
+
+    for (auto& item : items) {
+        if (item.name == name) {
+            cout << "Enter new price: ";
+            cin >> item.price;
+
+            if (item.price > 0 && item.price <= 10000) {
+                cout << "Price updated to " << item.price << "\n";
+            } else {
+                cout << "Invalid price.\n";
+            }
+            return;
+        }
+    }
+
+    cout << "Item not found.\n";
 }
