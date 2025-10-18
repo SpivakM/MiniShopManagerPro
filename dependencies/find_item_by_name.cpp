0971a878f648f84be53264f1830d13361861f4ed
valiilou <valeriiabilous07@gmail.com>
2025-10-18 12:28:16 +0300

    fix find_item_by_name and write delete_item

--- dependencies/find_item_by_name.cpp
+++ dependencies/find_item_by_name.cpp
@@ -5,25 +5,20 @@ int find_item_by_name(vector<Item>& items) {
 
     string name;
 
-    do {
-        cout << "Enter the name to search for: ";
-        getline(cin, name);
-        transform(name.begin(), name.end(), name.begin(), ::tolower);
-        if (name == "q") {
-            break;
+    cout << "Enter the name to search for: ";
+    getline(cin, name);
+    transform(name.begin(), name.end(), name.begin(), ::tolower);
+
+    for (int i = 0; i < items.size(); i++){
+        if (items[i].name == name){
+            cout << "\tname: " << name << endl;
+            cout << "\tprice: " << items[i].price << endl;
+            cout << "\tamount: " << items[i].amount << endl;
+            return i;
         }
+    }
 
-        for (int i = 0; i < items.size(); i++){
-            if (items[i].name == name){
-                cout << "\tname: " << name << endl;
-                cout << "\tprice: " << items[i].price << endl;
-                cout << "\tamount: " << items[i].amount << endl;
-                return i;
-            }
-        }
-
-        cout << "no such item" << endl;
-
-    } while (1);
+    cout << "no such item" << endl;
+    return -1;
 
 }
