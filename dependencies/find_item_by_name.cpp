9f50a24ffc30a386fa8ac0502bd088171a9ac837
valiilou <valeriiabilous07@gmail.com>
2025-10-19 20:06:45 +0300

    Fix find_item_by_name

--- dependencies/find_item_by_name.cpp
+++ dependencies/find_item_by_name.cpp
@@ -1,11 +1,13 @@
 #include "find_item_by_name.h"
 #include <algorithm>
+#include <limits>
 
 int find_item_by_name(vector<Item>& items) {
 
     string name;
 
     cout << "Enter the name to search for: ";
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
     getline(cin, name);
     transform(name.begin(), name.end(), name.begin(), ::tolower);
 
