ad8eef2bb40c486fbdfddec9e4c4f8b7eba07357
AskoldKushta <askold.kushna.shi.2025@lpnu.ua>
2025-10-19 12:47:45 +0300

    4

--- dependencies/find_item_with_max_price.cpp
+++ dependencies/find_item_with_max_price.cpp
@@ -12,11 +12,13 @@ void find_item_with_max_price(vector<Item>& items) {
         }
     }
 
+    cout << "Item(s) with max price:" << endl;
+
     for (int i=0; i<items.size(); i++){
 
         if (items[i].price == max_price){
 
-            cout << items[i].name << endl;
+            cout << items[i].name <<", price -  "<< items[i].price <<", currently available - "<< items[i].amount << endl;
         }
     }
 }
