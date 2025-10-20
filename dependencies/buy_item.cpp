2b9e5c864176a7ef0bd136ccbfe9cf8e9866072c
Arsen Stakhiv <arsen.stakhiv2305@gmail.com>
2025-10-20 12:35:32 +0300

    fix: fixed buy_item function

--- dependencies/buy_item.cpp
+++ dependencies/buy_item.cpp
@@ -2,22 +2,25 @@
 
 void buy_item(vector<Item>& items) {
     string n;
-    printf("Enter Item`s name:\n");
+    printf("Enter item's name: \n");
     cin >> n;
     int i = find_item_by_name(items, n);
-        if(items[i].amount > 0)
-        {
-            if(items[i].price == 0)
-            {
-                items[i].amount--;
-                printf("Item bought for free \n");
-            }
-            else
-            {
-                items[i].amount--;
-                printf("Item bought for: %u \n", items[i].price);
-            }        
-        }
-        else
-            printf("Item not avaible\n");
+
+	if (i != -1) {
+    	if (items[i].amount > 0)
+   		{
+			if(items[i].price == 0) {
+				printf("Item bought for free.\n");
+			}
+			else {
+				printf("Item bought for: %f UAH.\n", items[i].price);
+			}        
+
+			items[i].amount--;
+		} else {
+			printf("Item not avaible.\n");
+		}
+	} else {
+		printf("No item with such name exists.\n");
+	}
 }
