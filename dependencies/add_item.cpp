2806a52b18e45443b2ef4415e893e3292c0603fe
Disp <dimaspivak1602@gmail.com>
2025-10-20 11:58:02 +0300

    Fixed add_item

--- dependencies/add_item.cpp
+++ dependencies/add_item.cpp
@@ -5,17 +5,37 @@ void add_item(vector<Item>& items) {
     cout << "Enter the product name: ";
     cin.ignore();
     getline(cin, it.name);
-                
+
+    if (int index = find_item_by_name(items, it.name); index != -1) {
+        cout << "Item already exists. What amount of items should be added: ";
+        while (true) {
+            if (!(cin >> it.amount)) {
+                cin.clear();
+                cin.ignore(10000, '\n');
+                cout << "\nEnter numeric amount: ";
+                continue;
+            }
+
+            if (it.amount < 0) {
+                cout << "\nAmount cannot be negative: ";
+                continue;
+            }
+            break;
+        }
+        items[index].amount += it.amount;
+        return;
+    }
+
     cout << "Enter price: ";
-    while(true) {
-        if(!(cin >> it.price)) {
+    while (true) {
+        if (!(cin >> it.price)) {
             cin.clear();
             cin.ignore(10000, '\n');
             cout << "\nEnter numeric price: ";
             continue;
         }
 
-        if(it.price < 0) {
+        if (it.price < 0) {
             cout << "\nPrice cannot be negative: ";
             continue;
         }
@@ -24,14 +44,14 @@ void add_item(vector<Item>& items) {
 
     cout << "Enter amount of product: ";
     while (true) {
-        if(!(cin >> it.amount)) {
+        if (!(cin >> it.amount)) {
             cin.clear();
             cin.ignore(10000, '\n');
             cout << "\nEnter numeric amount: ";
             continue;
         }
 
-        if(it.amount < 0) {
+        if (it.amount < 0) {
             cout << "\nAmount cannot be negative: ";
             continue;
         }
--- dependencies/add_item.h
+++ dependencies/add_item.h
@@ -3,5 +3,6 @@
 	#include "includes.h"
 #endif
 #include "item.h"
+#include "find_item_by_name.h"
 
 void add_item(vector<Item>& items);
